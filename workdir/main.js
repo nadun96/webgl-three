a6d0763708ee50cafbc9b7adfd06429f626846ce
nadun96 <nadunudaraka@gmail.com>
2025-06-30 13:30:27 +0530

    webgl init

--- workdir/main.js
+++ workdir/main.js
@@ -2,10 +2,10 @@ function init() {
 	var scene = new THREE.Scene();
 
 	var camera = new THREE.PerspectiveCamera(
-		45,
-		window.innerWidth/window.innerHeight,
-		1,
-		1000
+		45, // Field of view in degrees
+		window.innerWidth/window.innerHeight, // Aspect ratio
+		1, // Near clipping plane
+		1000 // Far clipping plane
 	);
 	var renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
